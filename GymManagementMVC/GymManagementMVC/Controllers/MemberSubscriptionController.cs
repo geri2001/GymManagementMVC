0e4af676b5044cf49e72859998103a19baf8fc96
Ennio Gjondedaj <enio.gjondedaj99@gmail.com>
2023-03-04 13:39:32 +0100

    Member Subscription Added

---
+++ GymManagementMVC/GymManagementMVC/Controllers/MemberSubscriptionController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GymManagementMVC.Controllers
+{
+    public class MemberSubscriptionController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
